1b3231f8b8c5eb82b418aa069f97b6c249dec5a9
JBW <whitco670@jbw-mbp-215.attlocal.net>
2018-01-27 18:37:19 -0500

    retrieval complete

--- server.js
+++ server.js
@@ -1,26 +1,48 @@
 
-
+const express = require("express");
 const cheerio = require("cheerio");
 const request = require("request");
 
 const axios = require("axios");
 
+const db = require("./models");
+const PORT = 8080;
+const app = express();
 
-axios.get("http://www.velonews.com/category/news/").then(function(response) {
-	const $ = cheerio.load(response.data);
-	const results = [];
-	$("h3.article__title").each(function(i, element){
-		const title = $(element).text().trim();
-		//const summary = $(element).children().attr("p.article__excerpt");
-		const link = $(element).children().attr("href");
 
-		results.push({
-			title: title,
-			//summary: summary,
-			link: link
+
+app.get("/retrieve",function(req,res){
+	axios.get("http://www.velonews.com/category/news/").then(function(response) {
+		const $ = cheerio.load(response.data);
+		
+		$("h3.article__title").each(function(i, element){
+			const result = {};
+			result.title = $(this)
+				.text();
+			result.link = $(this)
+				.children()
+				.attr("href");	
+			//const summary = $(element).children().attr("p.article__excerpt");
+			db.Article.create(result)
+				.then(function(dbArticle){
+					console.log(dbArticle);
+				})
+				.catch(function(err){
+					return res.json(err);
+				});
+
+			});
+		res.send("Retrieval Complete")
 		});
 	});
-console.log(results);
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
-
-
