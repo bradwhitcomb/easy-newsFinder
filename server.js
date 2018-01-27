93a6cc802ce4d81a7961cad2edee8ecbed1a1b98
JBW <whitco670@jbw-mbp-215.attlocal.net>
2018-01-27 15:36:35 -0500

    scrap works

--- server.js
+++ server.js
@@ -1,55 +1,22 @@
-let express = require("express");
-let bodyparser = require("body-parser");
-let mongoose = require("mongoose");
-let logger = require("morgan");
 
-let cheerio = require("cheerio");
-let axios = require("axios");
 
-let PORT = 8080;
+const cheerio = require("cheerio");
+const request = require("request");
 
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-let app = express();
+request("http://www.velonews.com/category/news/", function(error, response, html){
+	const $ = cheerio.load(html);
+	const results = [];
+	$("h3.article__title").each(function(i, element){
+		const title = $(element).text().trim();
+		const link = $(element).children().attr("href");
 
-app.use(logger("dev"));
-app.use(bodyparser.urlencoded({extended: false}));
-app.use(express.static("public"));
-
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost",{
-	useMongoClient: true
+		results.push({
+			title: title,
+			link: link
+		});
+	});
+console.log(results);
 });
 
 
-app.get("/retrieve", function(req, res){
-	axios.get("https://www.cnbc.com/finance")
-	.then(function(response){
-		var $ = cherrio.load(response.data);
-
-		$(".cnbc-contents").each(function(i, element){
-			var result = {};
-
-			result.title = $(this)
-			.children("a")
-			.text ();
-		result.link = $(this)
-		    .children("a")
-		    .attr("href");
-		db.Article.create(result)
-			.then(function(dbArticle){
-				console.log(dbArticle);
-
-			})  
-		.catch(function(err){
-			return res.json(err);
-		});	  	
-	});
-		res.send("Completed the retrival")
-
-	});
-});
-
-app.listen(PORT, function(){
-	console.log("App running on port "+ PORT+ "!");
-})
